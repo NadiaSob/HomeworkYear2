41e1b51bdfefd9c59a19f956f8b272318dd5f0e6
NadiaSob <sobo_na@mail.ru>
2019-12-10 02:17:03 +0300

    Just started

---
+++ sem3/hw3/MyThreadPool/MyThreadPool/IMyTask.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyThreadPool
+{
+    public interface IMyTask<TResult>
+    {
+        /// <summary>
+        /// Возвращает true, если задача выполнена.
+        /// </summary>
+        bool IsCompleted { get; }
+
+        /// <summary>
+        /// Результат выполнения задачи.
+        /// </summary>
+        TResult Result { get; }
+
+        IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> newTask);
+    }
+}
---
+++ sem3/hw3/MyThreadPool/MyThreadPool/MyThreadPool.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace MyThreadPool
+{
+    public class MyThreadPool
+    {
+        /// <summary>
+        /// Очередь задач, принятых к исполнению.
+        /// </summary>
+        private ConcurrentQueue<Action> taskQueue;
+
+        /// <summary>
+        /// Возвращает связанный с ним CancellationToken.
+        /// </summary>
+        private CancellationTokenSource cancellationToken;
+
+        public MyThreadPool(int numberOfThreads)
+        {
+            taskQueue = new ConcurrentQueue<Action>();
+            cancellationToken = new CancellationTokenSource();
+        }
+
+        public void AddTask<TResult>(Func<TResult> supplier)
+        {
+            if (cancellationToken.Token.IsCancellationRequested)
+            {
+                throw new InvalidOperationException();
+            }
+
+            var myTask = new MyTask<TResult>(supplier);
+            taskQueue.Enqueue(myTask.Calculate);
+        }
+
+        /// <summary>
+        /// Завершает работу потоков.
+        /// </summary>
+        public void Shutdown()
+        {
+            cancellationToken.Cancel();
+        }
+
+        private class MyTask<TResult> : IMyTask<TResult>
+        {
+            public bool IsCompleted;
+
+            public TResult Result;
+
+            private Func<TResult> supplier;
+
+            public MyTask(Func<TResult> supplier)
+            {
+                this.supplier = supplier;
+            }
+
+            public void Calculate()
+            {
+                try
+                {
+                    Result = supplier();
+                }
+                catch(Exception exception)
+                {
+                    throw new AggregateException(exception);
+                }
+
+            }
+
+            public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> newTask)
+            {
+                
+            }
+        }
+    }
+}
