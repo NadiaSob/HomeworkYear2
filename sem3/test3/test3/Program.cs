5c686baccd38da39dae17c0368081d4abd43149d
NadiaSob <sobo_na@mail.ru>
2019-12-20 17:07:12 +0300

    Added task test4

---
+++ sem3/test3/test3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length == 0)
+            {
+                System.Console.WriteLine("Please enter an argument.");
+                return;
+            }
+
+            Console.WriteLine("File system directory:");
+            Console.WriteLine(args[0]);
+
+            var checksumCalculator = new ChecksumCalculator(args[0]);
+            Console.WriteLine($"Checksum is {checksumCalculator.Calculate()}");
+        }
+    }
+}
