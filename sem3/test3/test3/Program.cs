3ec4907ad1fc52e605a0ca9d09d81423e0a5c5ca
NadiaSob <sobo_na@mail.ru>
2019-12-20 17:15:53 +0300

    Added time checking

--- sem3/test3/test3/Program.cs
+++ sem3/test3/test3/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -16,11 +17,33 @@ namespace test3
                 return;
             }
 
-            Console.WriteLine("File system directory:");
-            Console.WriteLine(args[0]);
+            var path = args[0];
 
-            var checksumCalculator = new ChecksumCalculator(args[0]);
+            Console.WriteLine("File system directory:");
+            Console.WriteLine(path);
+
+            var checksumCalculator = new ChecksumCalculator(path);
             Console.WriteLine($"Checksum is {checksumCalculator.Calculate()}");
+
+            var multithreadedChecksumCalculator = new MultithreadedChecksumCalculator(path);
+
+            Stopwatch stopWatch = new Stopwatch();
+            checksumCalculator.Calculate();
+            TimeSpan ts = stopWatch.Elapsed;
+
+            string elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime of single-threaded calculation" + elapsedTime);
+
+            stopWatch = new Stopwatch();
+            multithreadedChecksumCalculator.Calculate();
+            ts = stopWatch.Elapsed;
+
+            elapsedTime = String.Format("{0:00}:{1:00}:{2:00}.{3:00}",
+                ts.Hours, ts.Minutes, ts.Seconds,
+                ts.Milliseconds / 10);
+            Console.WriteLine("RunTime of multi-threaded calculation" + elapsedTime);
         }
     }
 }
