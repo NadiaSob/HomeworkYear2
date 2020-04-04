392b274d35319761095669b0e79807a2ccf4825e
NadiaSob <sobo_na@mail.ru>
2020-04-04 18:03:58 +0300

    Added task 2.1 without tests

---
+++ sem4/hw2/hw2.1/hw2.1/EvenNumbersCount.fs
@@ -0,0 +1,10 @@
+﻿module EvenNumbersCount
+
+let evenNumbersCountMap list =
+    list |> List.map (fun n -> abs((n + 1) % 2)) |> List.sum
+
+let evenNumbersCountFilter list =
+    list |> List.filter (fun n -> n % 2 = 0) |> List.length
+
+let evenNumbersCountFold list = 
+    list |> List.fold (fun acc n -> abs((n + 1) % 2) + acc) 0
