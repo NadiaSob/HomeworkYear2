0e01a07a5690bd0372d959e2db60856384f70e35
NadiaSob <sobo_na@mail.ru>
2020-04-06 23:16:30 +0300

    Added task 2.3 without tests

---
+++ sem4/hw2/hw2.3/hw2.3/ExpressionTree.fs
@@ -0,0 +1,18 @@
+﻿module ExpressionTree
+
+/// Parsing tree for an arithmetic expression type.
+type ExpressionTree = 
+    | Number of int
+    | Addition of ExpressionTree * ExpressionTree
+    | Subtraction of ExpressionTree * ExpressionTree
+    | Multiplication of ExpressionTree * ExpressionTree
+    | Division of ExpressionTree * ExpressionTree
+
+/// Calculates the parsing tree value of an arithmetic expression.
+let rec calculate tree =
+    match tree with
+    | Number n -> n
+    | Addition(treeL, treeR) -> calculate(treeL) + calculate(treeR)
+    | Subtraction(treeL, treeR) -> calculate(treeL) - calculate(treeR)
+    | Multiplication(treeL, treeR) -> calculate(treeL) * calculate(treeR)
+    | Division(treeL, treeR) -> calculate(treeL) / calculate(treeR)
