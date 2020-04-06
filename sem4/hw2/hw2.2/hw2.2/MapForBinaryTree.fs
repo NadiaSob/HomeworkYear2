a373ea80bc0dfd6c8d7010215ff1d2e6eef9653b
NadiaSob <sobo_na@mail.ru>
2020-04-06 18:50:54 +0300

    Added task 2.2 without tests

---
+++ sem4/hw2/hw2.2/hw2.2/MapForBinaryTree.fs
@@ -0,0 +1,10 @@
+﻿module MapForBinaryTree
+
+type Tree<'a> =
+    | Node of 'a * Tree<'a> * Tree<'a>
+    | Empty
+
+let rec map func tree = 
+    match tree with
+    | Node(root, left, right) -> Node(func root, map func left, map func right)
+    | Empty -> Empty
