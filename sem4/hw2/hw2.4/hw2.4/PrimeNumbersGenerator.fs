fe8f59f3dc9e7629d0db9a63eea2c2936cc505c5
NadiaSob <sobo_na@mail.ru>
2020-04-07 22:01:04 +0300

    Added renamed file

---
+++ sem4/hw2/hw2.4/hw2.4/PrimeNumbersGenerator.fs
@@ -0,0 +1,18 @@
+﻿module PrimeNumbersGenerator
+
+/// Generates an infinite sequence of prime numbers.
+let generatePrimeNumbers () =
+    let isPrime n =
+        match n with
+        | _ when n <= 3 -> n > 1
+        | _ -> 
+            let rec isPrimeRecursive n i =
+                if i * i <= n then
+                    if n % i = 0 then
+                        false
+                    else
+                        isPrimeRecursive n (i + 1)
+                else
+                    true
+            isPrimeRecursive n 2
+    Seq.initInfinite (fun n -> n + 2) |> Seq.filter isPrime
